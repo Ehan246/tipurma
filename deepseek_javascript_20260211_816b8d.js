cb0942f4a8a460e731a1a8744d9a401a35c10d56
Ehan246 <81069206+Ehan246@users.noreply.github.com>
2026-02-11 12:38:56 +0600

    Add files via upload

---
+++ deepseek_javascript_20260211_816b8d.js
@@ -0,0 +1,38 @@
+// firebase-app.js
+// Firebase v9+ (Modular Version)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth, signInAnonymously, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getDatabase, ref, set, push, update, remove, onValue, get, runTransaction } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTyjzVYkwyeayKjZjfFNWHpLZJB5XOo1A",
+    authDomain: "itemsalesbd.firebaseapp.com",
+    databaseURL: "https://itemsalesbd-default-rtdb.firebaseio.com",
+    projectId: "itemsalesbd",
+    storageBucket: "itemsalesbd.firebasestorage.app",
+    messagingSenderId: "390468845332",
+    appId: "1:390468845332:web:7350cfdf25f23151688c01",
+    measurementId: "G-1REJXDEMSD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// Export for use in other files
+export { 
+    auth, 
+    db, 
+    signInAnonymously, 
+    onAuthStateChanged, 
+    signOut,
+    ref, 
+    set, 
+    push, 
+    update, 
+    remove, 
+    onValue, 
+    get, 
+    runTransaction 
+};
